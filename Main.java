03fe7210f6243f8b129499361488245aab6bfbe4
AndrewTC5 <andrew.creator05@gmail.com>
2024-09-25 14:38:27 -0600

    Pilas con stack

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        operacionesDePila();
+        buscadorStack();
     }
 
     //Historial de un motor de busqueda
@@ -21,7 +21,7 @@ public class Main {
             System.out.println("\nOperaciones:");
             System.out.println("1. Ingresar Busqueda");
             System.out.println("2. Eliminar Busqueda");
-            System.out.println("3. Ver cima (peek)");
+            System.out.println("3. Ver Ultima Busqueda");
             System.out.println("4. Ver Historial");
             System.out.println("5. Salir");
             System.out.print("Ingrese su opción: ");
@@ -50,8 +50,49 @@ public class Main {
                     System.out.println("Opción inválida.");
             }
         } while (opcion != 5);
-
         scanner.close();
+    }
 
+    public static void buscadorStack() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese la capacidad de busqueda: ");
+        int capacidad = scanner.nextInt();
+        scanner.nextLine();
+        BuscadorPila buscador = new BuscadorPila(capacidad);
+        int opcion;
+
+        do {
+            System.out.println("\nOperaciones:");
+            System.out.println("1. Ingresar Busqueda");
+            System.out.println("2. Eliminar Busqueda");
+            System.out.println("3. Ver ultima Busqueda");
+            System.out.println("4. Mostrar Historial");
+            System.out.println("5. Salir");
+            System.out.println("Seleccione una opcion");
+            opcion = scanner.nextInt();
+            scanner.nextLine();
+            switch (opcion) {
+                case 1:
+                    System.out.print("Ingrese Su Busqueda: ");
+                    String elemento = scanner.nextLine();
+                    buscador.agregar(elemento);
+                    break;
+                case 2:
+                    buscador.quitar();
+                    break;
+                case 3:
+                    buscador.limite();
+                    break;
+                case 4:
+                    buscador.mostrarPila();
+                    break;
+                case 5:
+                    System.out.println("Saliendo...");
+                    break;
+                default:
+                    System.out.println("No valido");
+            }
+        } while (opcion != 5);
+        scanner.close();
     }
 }
